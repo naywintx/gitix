2510e58ca29f4dab97c7b3b136426bf2835bd9a6
Friedger Müffke <mail@friedger.de>
2019-09-14 13:07:44 +0200

    update meta in index.html

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import AppContainer from "./components/App-Container";
 import { withRouter } from "react-router-dom";
 import { checkIsSignedIn, loadUserData } from "./lib/blockstack";
-
+import { Helmet } from "react-helmet";
 const NotSignedIn = {
   checking: false,
   user: null,
@@ -22,8 +22,24 @@ const App = () => {
       }
     });
   }, []);
+  const title = "gitix.org";
+  const metaDescription = "Decentralized git profiles";
+  const img = "https://app.gitix.org/favicon.ico";
   return (
     <UserStateContext.Provider value={signIn}>
+      <Helmet>
+        <title>{title}</title>
+        <meta name="description" content={metaDescription} />
+        <meta name="og:title" content={title} />
+        <meta name="og:description" content={metaDescription} />
+        <meta name="og:type" content="website" />
+        <meta name="og:image" content={img} />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:creator" content="gitix.org" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={metaDescription} />
+      </Helmet>
+
       <AppContainer />
     </UserStateContext.Provider>
   );
